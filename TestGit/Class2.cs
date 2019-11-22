f1cfc39e4336e90006356668dad65f9b5cf2af5c
trangluu <luudesmottes.trang@gmail.com>
2019-11-22 11:28:27 +0100

    Add class2 and use it

---
+++ TestGit/Class2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestGit
+{
+    public class Class2
+    {
+        private string alt1;
+        private string alt2;
+
+        //public string Alt1
+        //{
+        //    get { return alt1; }
+        //    set { alt1 = value; }
+        //}
+
+
+        //public string Alt2
+        //{
+        //    get { return alt2; }
+        //    set { alt2 = value; }
+        //}
+
+        public string Alt1 { get => alt1; set => alt1 = value; }
+        public string Alt2 { get => alt2; set => alt2 = value; }
+
+    }
+}
--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -13,6 +13,10 @@ namespace TestGit
             Class1 class1 = new Class1();
             class1.Alt1 = "hello";
             class1.Alt2 = "World";
+
+            Class2 class2 = new Class2();
+            class2.Alt1 = "hello";
+            class2.Alt2 = "World";
         }
     }
 }
